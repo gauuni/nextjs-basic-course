8e0ca046f158ef820cc61fdd2f88c733332d72ef
Khoi Nguyen <nguyenkhoinguyen082444@gmail.com>
2023-12-14 19:45:12 +0900

    mod. add components

---
+++ components/landing_page/Opportunity.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Opportunity = (props: Props) => {
+  return <div>Opportunity</div>;
+};
+
+export default Opportunity;
---
+++ components/landing_page/Variant.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Variant = (props: Props) => {
+  return <div>Variant</div>;
+};
+
+export default Variant;
