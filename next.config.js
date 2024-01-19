5b4cac800f046aa6685efb4b5ead5649fa993055
Nguyen Khoi <gauuniios@icloud.com>
2024-01-19 15:43:57 +0900

    mod. update next config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,12 @@
-/** @type {import('next').NextConfig} */
+/**
+* @type {import('next').NextConfig}
+*/
 const nextConfig = {
-  reactStrictMode: true
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: "/movie/home",
-  //       missing: [
-  //         {
-  //           type: "cookie",
-  //           key: "session_id"
-  //         }
-  //       ],
-  //       destination: "/login",
-  //       permanent: false
-  //     }
-  //   ];
-  // }
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix: './',
 };
 
-module.exports = nextConfig;
+export default nextConfig;
