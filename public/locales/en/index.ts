45173ce7779a3d2a6fb9cef6066647fe9b890d6c
Nguyen Khoi <gauuniios@icloud.com>
2024-03-21 18:36:49 +0900

    remove localize

--- public/locales/en/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as commonEN } from './common.json'
-export { default as errorCodeEN } from './errorCode.json'
-export { default as chartTypeEN } from './chartType.json'
-export { default as heartEN } from './heart.json'
--- public/locales/ja/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as commonJA } from './common.json'
-export { default as errorCodeJA } from './errorCode.json'
-export { default as chartTypeJA } from './chartType.json'
-export { default as heartJA } from './heart.json'
